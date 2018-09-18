5875dd09b4c8fe091a3bd35cc2087606cfef4159
liulin <liulin1987315@gmail.com>
2018-09-18 19:41:25 +1000

    Add react router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Todos } from './Todos';
 import AddTodoForm from './AddForm';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
 
 class App extends Component {
   state = {
@@ -30,11 +35,18 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-        <AddTodoForm addTodo={this.addTodo} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+
+          <h1 className="center blue-text">Todo's</h1>
+          <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+          <AddTodoForm addTodo={this.addTodo} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
