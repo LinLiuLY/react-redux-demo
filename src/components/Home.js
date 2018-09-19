86fd94a4685289c1fdf2c2f3e97bf6be70469d8d
liulin <liulin1987315@gmail.com>
2018-09-19 20:05:28 +1000

    Using Axios.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home page</h4>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    if (posts.length <= 0)
+      return (
+        <div className="container">
+          <h4 className="center">Home page</h4>
+        </div>
+      );
+
+    const postContent = post => (
+      <div className="post card" key={post.id}>
+        <div className="card-content">
+          <span className="card-title">{post.title}</span>
+          <p>{post.body}</p>
+        </div>
+      </div>
+    );
+
+    return posts.map(post => postContent(post));
+  }
+}
 
 export default Home;
--- src/hoc/Rainbow.js
+++ src/hoc/Rainbow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Rainbow = WrappedComponent => {
   const colors = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
