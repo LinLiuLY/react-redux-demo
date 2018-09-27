d04d7df93e9b2c8a42e0f4192492aefa81b6e857
liulin <liulin1987315@gmail.com>
2018-09-27 20:33:10 +1000

    Map Dispatch To Props.

--- src/components/post/LocalPost.js
+++ src/components/post/LocalPost.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class LocalPost extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/posts');
+  };
+
   render() {
     const { post } = this.props;
     if (!post) return <div className="center">Loading post ...</div>;
@@ -11,6 +16,11 @@ class LocalPost extends Component {
       <div className="post">
         <h4 className="center">This is Title {title}</h4>
         <p>{body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            delete post
+          </button>
+        </div>
       </div>
     );
   }
@@ -19,8 +29,19 @@ class LocalPost extends Component {
 const mapStateToProps = (state, ownProps) => {
   const id = ownProps.match.params.post_id;
   return {
-    post: state.posts.find(post => post.id === id)
+    post: state.posts.find(post => String(post.id) === String(id))
   };
 };
 
-export default connect(mapStateToProps)(LocalPost);
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePost: id => {
+      dispatch({ type: 'DELETE_POST', id: id });
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LocalPost);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 const initState = {
+  users: [],
   posts: [
     { id: 1, title: 'one', body: 'body one' },
     { id: 2, title: 'two', body: 'body two' }
@@ -6,6 +7,13 @@ const initState = {
 };
 
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'DELETE_POST') {
+    let newPosts = state.posts.filter(post => post.id !== action.id);
+    return {
+      ...state,
+      posts: newPosts
+    };
+  }
   return state;
 };
 
